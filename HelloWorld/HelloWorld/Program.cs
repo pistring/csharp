afc48b5878a01cbe1be4f642b7d8b8bed2f74dde
pistring <116700542+pistring@users.noreply.github.com>
2024-01-10 17:35:29 +0600

    First Commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Input your name: ");
+var yourFName = Console.ReadLine();
+var yourLName = Console.ReadLine();
+Console.WriteLine("Your name is: " + yourFName +" " + yourLName );
