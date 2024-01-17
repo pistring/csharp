f5bca8d9e66171590c131d8bd4d82575bb90e6b6
pistring <116700542+pistring@users.noreply.github.com>
2024-01-17 14:00:51 +0600

    running now

--- ifstate/Program.cs
+++ ifstate/Program.cs
@@ -1,6 +1,6 @@
 ﻿Random dice = new Random();
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+int roll1 = dice.Next(1, 6);
+int roll2 = dice.Next(1, 6);
+int roll3 = dice.Next(1, 6);
 int total = roll1 + roll2 + roll3;
-Console.Writeline($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
