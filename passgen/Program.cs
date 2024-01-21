6b546b3064aedab87eb34dd53c06c6bbc7a52e94
pistring <116700542+pistring@users.noreply.github.com>
2024-01-21 09:37:03 +0600

    Cleared

---
+++ passgen/Program.cs
@@ -0,0 +1,10 @@
+﻿var CharSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLM0123456789!@#$%&*+=";
+Random rand = new Random();
+var RandPass ="";
+int size =13;
+for (int i=1; i < size; i++)
+{
+	int x = rand.Next(CharSet.Length);
+	RandPass = RandPass + CharSet[x];
+}
+Console.WriteLine("Temp Generated Pass : "+ RandPass);
